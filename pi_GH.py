0c349bba0533dcaa7b484078d19bfa558980239a
Nguyễn Đức Hùng <79887728+hung202@users.noreply.github.com>
2023-10-12 16:02:09 +0700

    ff

--- pi_GH.py
+++ pi_GH.py
@@ -11,6 +11,7 @@ import requests
 import smbus
 import Adafruit_SSD1306
 from PIL import Image, ImageDraw, ImageFont
+from datetime import datetime
 
 
 display = Adafruit_SSD1306.SSD1306_128_64(rst=None)
@@ -32,7 +33,7 @@ flag2 = True
 THINGSBOARD_HOST = 'demo.thingsboard.io'
 ACCESS_TOKEN = 'yGh3vVrmRLrv0eQHRPtK'
 
-
+# We assume that all GPIOs are LOW
 gpio_state = {18: False}
 fan_status={27: False}
 pump_status={16: False}
@@ -40,6 +41,7 @@ pump_status={16: False}
 pin = 18
 pin2 = 27
 pin4 = 16
+
 GPIO.setwarnings(False)
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(pin, GPIO.OUT)
@@ -49,20 +51,19 @@ GPIO.setup(pin4, GPIO.OUT)
 
 def send_message(name, data, device):
     return requests.post(
-            "https://api.mailgun.net/v3/sandboxa23eea2d073241058ce1703da7ba11a2.mailgun.org/messages",
-            auth=("api", "becd0fee112532e388a257f368b3c942-50f43e91-9541815d"),
-            data={"from": "Me <mailgun@becd0fee112532e388a257f368b3c942-50f43e91-9541815d>",
+            "https://api.mailgun.net/v3/sandboxa39239f334e243128e268cd01ec9886e.mailgun.org/messages",
+            auth=("api", "7d0563532f3f57142153d04eb3324403-5465e583-49d5fdf1"),
+            data={"from": "Me <mailgun@sandboxa39239f334e243128e268cd01ec9886e.mailgun.org>",
                     "to": ["artaimer00@gmail.com "],
                     "subject": "PKA GreenHouse",
-                    "html": "<html> " + name + " is exceeding the threshold: " + data + " <br></br>"
-                    + device + " is turned on <br></br> Dashboard PKA-GH to see more:  https://bom.so/dTw4mA</html>" })
+                    "html": "<html> " + name + " đã vượt ngưỡng cho phép " + data + " <br></br>"
+                    + device + " đã được bật để khắc phục <br></br>  Đến Dashboard PKA-GH để xem chi tiết: <br></br> https://demo.thingsboard.io/dashboard/094bd510-4ccf-11ee-af4c-6dae12af61b6?publicId=d009c120-4d1b-11ee-af4c-6dae12af61b6 </html>" })
 
 # The callback for when the client receives a CONNACK response from the server.
 def on_connect(client, userdata, rc, *extra_params):
     print('Connected with result code ' + str(rc))    # Subscribing to receive RPC requests
     client.subscribe('v1/devices/me/rpc/request/+')
     # Sending current GPIO status
-
     client.publish('v1/devices/me/attributes', get_gpio_status(), 1)
     client.publish('v1/devices/me/attributes', get_fan_status(), 1)
     client.publish('v1/devices/me/attributes', get_pump_status(), 1)
@@ -83,36 +84,45 @@ def on_message(client, userdata, msg):
         client.publish('v1/devices/me/attributes', get_gpio_status(), 1)
 
     elif data['method'] == 'setFanOn':
+        # Update GPIO status and reply
         set_fan_status(data['params'])
         client.publish(msg.topic.replace('request', 'response'), get_fan_status(), 1)
         client.publish('v1/devices/me/attributes', get_fan_status(), 1)
     elif data['method'] == 'setFanOff':
+        # Update GPIO status and reply
         set_fan_status(data['params'])
         client.publish(msg.topic.replace('request', 'response'), get_fan_status(), 1)
         client.publish('v1/devices/me/attributes', get_fan_status(), 1)
         
     elif data['method'] == 'setWpumpOff':
+        # Update GPIO status and reply
         set_pump_status(data['params'])
         client.publish(msg.topic.replace('request', 'response'), get_pump_status(), 1)
         client.publish('v1/devices/me/attributes', get_pump_status(), 1)
     elif data['method'] == 'setWpumpOn':
+        # Update GPIO status and reply
         set_pump_status(data['params'])
         client.publish(msg.topic.replace('request', 'response'), get_pump_status(), 1)
         client.publish('v1/devices/me/attributes', get_pump_status(), 1)
         
     elif data['method'] == 'setHdoorOff':
+        # Update GPIO status and reply
         servo.angle= -90
         door_status={22: False}
+        
         client.publish('v1/devices/me/attributes', json.dumps(door_status), 1)
+        
     elif data['method'] == 'setHdoorOn':
+        # Update GPIO status and reply
         servo.angle= 90
         door_status={22: True}
+        
         client.publish('v1/devices/me/attributes', json.dumps(door_status), 1)
         
 
 
 def get_gpio_status():
-    
+    # Encode GPIOs state to json
     return json.dumps(gpio_state)
 
 def set_gpio_status(status):    
@@ -124,18 +134,24 @@ def set_gpio_status(status):
 
 
 def get_fan_status():
+    # Encode GPIOs state to json
     return json.dumps(fan_status)
     
 def set_fan_status(status):
+    # Output GPIOs state
     GPIO.output(pin2, GPIO.HIGH if status else GPIO.LOW)
+    # Update GPIOs state
     fan_status[27] = status
 
 
 def get_pump_status():
+    # Encode GPIOs state to json
     return json.dumps(pump_status)
 
 def set_pump_status(status):
+    # Output GPIOs state
     GPIO.output(pin4, GPIO.HIGH if status else GPIO.LOW)
+    # Update GPIOs state
     pump_status[16] = status
 
 
@@ -153,6 +169,7 @@ INTERVAL=2
 sensor_data = {'Temperature1': 0, 'Humidity1': 0,'Temperature2': 0, 'Humidity2': 0 }
 soil_data = {'Soilmoisture':0 }
 lux_data ={'Lux':0}
+time_data ={'Time':0}
 
 local_data = {'latitude': 21.01908694793996, 'longitude':105.64920567296556}
 
@@ -176,9 +193,17 @@ try:
     display.display()  # write display buffer to the physical display
     displayWidth = display.width  # get width of display
     displayHeight = display.height  # get height of display
-    font = ImageFont.load_default()  # load and set default font
+    font = ImageFont.load_default()
+    #font_l = ImageFont.truetype('Montserrat-Light.ttf', 12)# load and set default font
 
     while True:
+        
+        client.publish('v1/devices/me/telemetry', json.dumps(local_data), 1)
+        
+        now = datetime.now()
+        time_data['Time'] = now.strftime("%H:%M")
+        
+        client.publish('v1/devices/me/telemetry', json.dumps(time_data), 1)
         
         image = Image.new('1', (displayWidth, displayHeight))  # create graphics library image buffer
         draw = ImageDraw.Draw(image)  # create drawing object
@@ -200,9 +225,7 @@ try:
 
         # Sending humidity and temperature data to ThingsBoard
         client.publish('v1/devices/me/telemetry', json.dumps(sensor_data), 1)
-
-        client.publish('v1/devices/me/telemetry', json.dumps(local_data), 1)
-         
+        
         print(u"TemperatureI: {:g}\u00b0C, HumidityI: {:g}%".format(temperature, humidity))
         print(u"TemperatureO: {:g}\u00b0C, HumidityO: {:g}%".format(temperature2, humidity2))
 
@@ -211,26 +234,22 @@ try:
          
 
            data= str(temperature)
-           name= "Temperature"
-           device="Fan"
+           name= "Nhiệt độ"
+           device="Quạt"
            if flag:
               request= send_message(name, data, device)
               print ('Status: '+format(request.status_code))
               print ('Body:'+ format(request.text))
-              
               flag = False
            
         else :
+           flag = True         
            
-           flag = True
-           
+
         moisture_value = am.read_adc(0)
         soil_per = int(100-(moisture_value * 100 / 1023))
         print("Recorded moisture value is %s percentage" % soil_per)
-
-
         soil_data['Soilmoisture'] = soil_per
-
         client.publish('v1/devices/me/telemetry', json.dumps(soil_data), 1)
 
         
@@ -239,8 +258,8 @@ try:
            
 
            data= str(soil_per)
-           name= "Soil moisture"
-           device="Waterpumps"
+           name= "Độ ẩm đất"
+           device="Máy bơm"
            if flag1:
               request= send_message(name, data, device)
               print ('Status: '+format(request.status_code))
@@ -250,43 +269,66 @@ try:
          
         elif moisture_value < 350 :
            flag1= True
-           
 
          
         lightLevel=readLight()
         print("Light Level : " + format(lightLevel,'.2f') + " lx")
-        lightLevel = round(lightLevel, 2)
-
-
+        lightLevel = int(lightLevel)
         lux_data['Lux'] = lightLevel
         
         client.publish('v1/devices/me/telemetry', json.dumps(lux_data), 1)
 
         if lightLevel > 9000:
            data= str(lightLevel)
-           name= "Light intensity"
-           device="Heaven door"
+           name= "Cường độ ánh sáng"
+           device="Cửa trời"
            if flag2:
               request= send_message(name, data, device)
               print ('Status: '+format(request.status_code))
               print ('Body:'+ format(request.text))
+                               
               flag2 = False
 
         else:
            flag2 = True
-  
+           
+           
+        led_s = GPIO.input(pin)
+        if led_s == GPIO.HIGH:
+            draw.text((1, 40), "Led: On ", fill=225)
+        else:
+            draw.text((1, 40), "Led: Off ", fill=225)
+        
+        fan_s = GPIO.input(pin2)
+        if fan_s == GPIO.HIGH:
+            draw.text((60, 40), "Fan: On ", fill=225)
+        else:
+            draw.text((60, 40), "Fan: Off ", fill=225)
+            
+        wp_s = GPIO.input(pin4)
+        if wp_s == GPIO.HIGH:
+            draw.text((1, 50), "Wp: On ", fill=225)
+        else:
+            draw.text((1, 50), "Wp: Off ", fill=225)
+            
+        if servo.angle == 90:
+            draw.text((60, 50), "Hd: On ", fill=225)
+        else:
+            draw.text((60, 50), "Hd: Off ", fill=225)
+            
+    
         
             
         draw.text((1, 0), "°C I: "+ str(int(temperature)), fill=225)
-        draw.text((1, 10), "°C O: "+ str(int(temperature2)), fill=225)
-        draw.text((60, 0), "Hr% I: "+ str(int(humidity)), fill=225)
-        draw.text((60, 10), "Hr% O: "+ str(int(humidity2)), fill=225)
-        draw.text((30, 20), "W %: "+ str(soil_per), fill=225)
-        draw.text((30, 30), "Lx : "+ str(lightLevel), fill=225)
+        draw.text((60, 0), "°C O: "+ str(int(temperature2)), fill=225)
+        draw.text((1, 10), "H% I: "+ str(int(humidity)), fill=225)
+        draw.text((60, 10), "H% O: "+ str(int(humidity2)), fill=225)
+        draw.text((1, 20), " W% : "+ str(soil_per), fill=225)
+        draw.text((60, 20), "Lx : "+ str(lightLevel), fill=225)
         
    
-        display.image(image)  
-        display.display()  
+        display.image(image)  # set display buffer with the image buffer
+        display.display()  # write display buffer to the physical display
 
 
         next_reading += INTERVAL
